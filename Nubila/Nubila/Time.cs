20502f15e8b972fdfbc6bf1f9f377d3f692b9b95
Robert MacWha <trebor.ahwcam@gmail.com>
2024-08-14 15:04:52 -0400

    clear cs implementation

--- Nubila/Nubila/Time.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nubila
-{
-    static class Time
-    {
-        public static float DeltaTime = 0;
-        public static float ElapsedTime = 0;
-        public static int Ticks = 0;
-    }
-}
